d42a839d390fb4e2ac1abdd1678952371ef0f859
rom4chick <obereg2005.2002@gmail.com>
2021-06-01 09:16:14 +0300

    added beta-version of binary tree algorithm

--- maze.js
+++ maze.js
@@ -1,11 +1,11 @@
 const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
 
-async function main () {
+async function oldosBrogerAlgorithm () {
     startDiagnostics();
 
-    while(!isMazeDone()) {
+    while (!isMazeDone()) {
         for (const eater of eaters) {
-            moveEater(eater);
+            moveEaterOldosBroger(eater);
         }
         if (withAnimation) {
             drawMaze();
@@ -40,10 +40,7 @@ const drawMaze =  () => {
     context.beginPath();
     context.rect(0, 0, canvas.width, canvas.height);
     context.fillStyle = backgroundColor;
-    context.fill();
-
-
-    
+    context.fill(); 
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x  < columns; x++) {
             const color = matrix[y][x] ? freeCellsColor : wallColor;
@@ -63,27 +60,23 @@ const drawEater = eater => {
     context.fill();
 }
 
-const moveEater = eater => {
+const moveEaterOldosBroger = eater => {
     let directions = [];
 
     if (eater.x > 0) {
         directions.push([-2, 0]);
-        counter++;
     }
 
     if (eater.x < columns - 1) {
         directions.push([2, 0]);
-        counter++;
     }
 
     if (eater.y > 0) {
         directions.push([0, -2]);
-        counter++;
     }
 
     if (eater.y < rows - 1) {
         directions.push([0, 2]);
-        counter++;
     }
 
     const [dx, dy] = getRandomItem(directions);
@@ -94,13 +87,11 @@ const moveEater = eater => {
     if (!matrix[eater.y][eater.x]) {
         matrix[eater.y][eater.x] = 1;  
         matrix[eater.y - dy / 2][eater.x - dx / 2] = 1;  
+        counter++;
     }
 }
 
-const getRandomItem = array => {
-    const index = Math.floor(Math.random() * array.length);
-    return array[index];
-}
+const getRandomItem = array => array[Math.floor(Math.random() * array.length)];
 
 const download = (content, fileName, contentType) => {
     let a = document.createElement("a");
@@ -158,6 +149,7 @@ const startDiagnostics = () => {
 const pauseDiagnostics = () => {
   watch.classList.add('paused');
   clearInterval(timer);
+  operationCounter.innerHTML = counter;
 };
 
 const resetWatch = () => {
@@ -166,12 +158,74 @@ const resetWatch = () => {
 	milliseconds = 0;
 	watch.innerHTML = '00:00:00:00';
 };
+
+function binaryTreeMove() {
+    for (let y = 0; y < rows; y += 2) {
+        for (let x = 0; x < columns; x += 2) {
+            let directions = [];
+
+            matrix[y][x] = 1;
+
+            if (x < columns - 1) {
+                directions.push([2, 0]);
+            }
+        
+            if (y < rows - 1) {
+                directions.push([0, 2]);
+            }
+        
+            if (directions.length !== 0) {
+                let [dx, dy] = getRandomItem(directions);
+               
+                matrix[y + dy][x + dx] = 1; 
+                matrix[(y + dy) - dy / 2][(x + dx) - dx / 2] = 1 
+                counter++;
+                if (withAnimation) {
+                    drawMaze();
+                    // let delay = 1000;
+                    // await delay(delay);
+                }
+                
+            }            
+        }
+    }
+    if (isMazeDone()) {
+        drawMaze();
+    }  else {
+        console.log("error");
+    } 
+}
+
+async function binaryTreeAlgorithm() {
+    startDiagnostics();
+    binaryTreeMove();
+     
+
+}
+
+const creationChoice = wayToCreate => {
+    switch (wayToCreate) {
+        case "1": {
+            oldosBrogerAlgorithm();
+            break;
+        }
+        case "2": {
+            let wayToMove = prompt("Введите то, каким способом вы хотели бы двигаться \n1 - северо-запад, \n2 - северо-восток, \n3 - юго-запад, \n4 - юго - восток");
+            binaryTreeAlgorithm();
+            break;
+        }
+        default:
+            alert("Был введён неверный способ генерации");
+            break;
+    }
+}
  
 const cellSize = 10;
 const canvasPadding = 5;
 const wallColor = '#000';
 const freeCellsColor = '#fff';
 const backgroundColor = '#333';
+let wayToCreate = prompt("Каким способом вы хотите сгенерировать лабиринт\n 1 - методом Олдоса-Брогера\n 2 - методом Бинарного дерева");
 const columns = prompt("Введите кол-во колонок");
 const rows = prompt("Введите кол-во строк");
 const eaterColor = "#FF5733"
@@ -181,8 +235,8 @@ let counter = 0;
 let withAnimation = prompt("Вы хотите создание алгоритма с анимацией или без?");
 withAnimation === "1" ? withAnimation = true : withAnimation = false;
 // if (withAnimation) {
-//     document.getElementsByClassName('item').classList.remove('invicible');
-// }
+//     document.getElementsByClassName('item').classList.remove('invicible'); }
+
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
@@ -198,4 +252,7 @@ for (let i = 0; i < eatersAmount; i++) {
 
 matrix[0][0] = 1;
 
-main();
+creationChoice(wayToCreate)
+
+
+
